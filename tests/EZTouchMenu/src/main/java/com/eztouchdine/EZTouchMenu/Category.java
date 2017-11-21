f414fff0056db5e636aad76813cd55fc26417e60
RajiBabu <rajipecster@gmail.com>
2017-11-20 22:47:03 -0800

    all tests Code

---
+++ tests/EZTouchMenu/src/main/java/com/eztouchdine/EZTouchMenu/Category.java
@@ -0,0 +1,36 @@
+package com.eztouchdine.EZTouchMenu;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+public class Category {
+	
+	
+	private String hostRestURLPrefix = null;
+
+	public Category(String hostName) {
+		Client.create();
+		this.hostRestURLPrefix = "https://" + hostName + "/";
+	}
+
+	  public String testCategory(String searchParam) {
+		   Client client = Client.create();
+		   
+		WebResource webResource = client.resource(hostRestURLPrefix+searchParam);
+			ClientResponse response = webResource.accept("application/json").get(
+					ClientResponse.class);
+			if (response.getStatus() != 200) {
+				throw new RuntimeException("HTTP ERROR RESPONSE CODE : "
+						+ response.getStatus());
+			}
+			String output = response.getEntity(String.class);
+			System.out.println(output);
+			return output;
+	   }
+	
+	
+	
+	
+
+}
